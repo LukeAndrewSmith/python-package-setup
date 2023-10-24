6233b437207fb39211a89dae936a1ce15b42634e
Luke.Smith <luke.smith@bluewin.ch>
2023-10-24 17:46:34 +0200

    Adding logging

--- src/a_package/a_script/say_hello.py
+++ src/a_package/a_script/say_hello.py
@@ -1,9 +1,13 @@
 from a_package.a_script.args import get_args
+import logging
+import coloredlogs
+
+coloredlogs.install(level=logging.INFO)
 
 
 def say_hello():
     cfg = get_args()
     print("Hello: this is a command line script")
     if cfg.int_to_print > 0:
+        logging.info("Processing user input")
         print(f"You asked me to print: {cfg.int_to_print}")
-        print("Why would you do such a thing?")
